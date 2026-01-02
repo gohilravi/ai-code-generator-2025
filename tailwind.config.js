9da52456aaa5d0945a95b674f7ee50eba141cf12
Krunal Dubey <123437095+krunaldubey@users.noreply.github.com>
2026-01-02 20:16:03 +0530

    feat: convert tailwind config to ES module syntax
    Replace CommonJS require with ES6 import for tailwindcss-animate plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -83,5 +85,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
